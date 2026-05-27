ed9d2a2e83f43bde67788c17a0bf9bb313f1def0
jeffersonveloz-lab <jeffersonvelozh@gmail.com>
2026-05-27 11:11:25 -0500

    Otros cambios

--- Frontend/src/app/feactures/pages/public/games-component/games-component.ts
+++ Frontend/src/app/feactures/pages/public/games-component/games-component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-games-component',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './games-component.html',
   styleUrl: './games-component.scss',
 })
